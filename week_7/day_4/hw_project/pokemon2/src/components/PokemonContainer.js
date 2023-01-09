d859d1ad3ad4de885c31f19134dc26d5b20c37b4
ljones-11 <116349877+ljones-11@users.noreply.github.com>
2023-01-09 13:35:21 +0000

    wk8d1 testing lab initial commit

--- week_7/day_4/hw_project/pokemon2/src/components/PokemonContainer.js
+++ week_7/day_4/hw_project/pokemon2/src/components/PokemonContainer.js
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react'
-import Mon from './Mon'
 
 const PokemonContainer = function() {
 
     const [pokemon, setPokemon] = useState([])
+    const [monSelected, setMonSelected] = useState(null)
 
     useEffect (() => {
         getPokemon();
@@ -14,19 +14,35 @@ const PokemonContainer = function() {
         fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
         .then(res => res.json())
         // .then(pokemonData => console.log(pokemonData))
-        .then(pokemonData => setPokemon(pokemonData.results))
+        .then(data => {
+            console.log(data)
+            const pokemonNames = data.results.map((mon) => {
+                return  mon.name     
+            })
+                setPokemon(pokemonNames)
+        })
     }
 
-    const pokemonNameNum = pokemon.map((mon, index) => {
-        return  <Mon key = {index} name = {mon.name} number = {index+1} />
-                
+    const monNames = pokemon.map(name => (
+        <option key={name} value={name}>{name}</option>
+    ))  
+    
+    const handleChange = function(event) {
+        const name = event.target.value;
+        setMonSelected(name)
+    }
 
-    })
+
 
 
     return (
-        <div className='pokemon-card'>
-            {pokemonNameNum}
+        <div>
+            <div>
+                <select onChange={handleChange}>
+                    <option selected>-- Choose a Pokemon --</option>
+                    {monNames}
+                </select>
+            </div>
         </div>
     );
 
